6b6891f3573880f98bb8674a3a96a0c048797c48
Trac Nguyen <nguyendtrac@gmail.com>
2018-09-18 05:43:35 +0000

    automatically backed up by learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,18 +1,7 @@
 class PostsController < ApplicationController
 
   def index
-    @authors = Author.all
-
-    if !params[:author].blank?
-      @posts = Post.where(author: params[:author])
-    elsif !params[:date].blank?
-      if params[:date] == "Today"
-        @posts = Post.where("created_at >=?", Time.zone.today.beginning_of_day)
-      else
-        @posts = Post.where("created_at <?", Time.zone.today.beginning_of_day)
-      end
-    else
-      @posts = Post.all
+    @posts = Post.all
   end
 
   def show
