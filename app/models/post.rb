d4aee053e1bf380f548303f05d6de0d4d3ddab00
Trac Nguyen <nguyendtrac@gmail.com>
2018-09-18 06:12:15 +0000

    automatically backed up by learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -12,6 +12,14 @@ class Post < ActiveRecord::Base
     where(author: author_id)
   end
 
+  def self.from_today
+    where("created_at >=?", Time.zone.today.beginning_of_day)
+  end
+   
+  def self.old_news
+    where("created_at <?", Time.zone.today.beginning_of_day)
+  end
+
   def is_title_case
     if title.split.any?{|w|w[0].upcase != w[0]}
       errors.add(:title, "Title must be in title case")
