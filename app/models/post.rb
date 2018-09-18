b73f52a2e68b3244c04335dfa3d7a3396231a844
Trac Nguyen <nguyendtrac@gmail.com>
2018-09-18 06:11:54 +0000

    automatically backed up by learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,17 +1,17 @@
 class Post < ActiveRecord::Base
 
-  validate :is_title_case 
-  before_validation :make_title_case 
+  validate :is_title_case
+  before_validation :make_title_case
   belongs_to :author
 
   #put new code here
 
   private
-  
+
   def self.by_author(author_id)
     where(author: author_id)
   end
-  
+
   def is_title_case
     if title.split.any?{|w|w[0].upcase != w[0]}
       errors.add(:title, "Title must be in title case")
